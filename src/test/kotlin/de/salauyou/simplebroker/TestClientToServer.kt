9b7ef208c914d6b954309d6c4923a72ab5d97b4b
Salauyou <info@salauyou.ru>
2025-11-13 15:21:12 +0100

    Client#sync and subscription acknowledgement

--- src/test/kotlin/de/salauyou/simplebroker/TestClientToServer.kt
+++ src/test/kotlin/de/salauyou/simplebroker/TestClientToServer.kt
@@ -13,10 +13,36 @@ class TestClientToServer {
         server.start()
         val client = Client("localhost", 7000)
         client.start()
-        client.subscribeTopic("Test topic") {}
-        Thread.sleep(100)
-        server.stop()
+        client.subscribeTopic("Test topic", {}).get()
         client.stop()
+        server.stop()
+    }
+
+    @Test
+    fun `client requests sync for topic with no subscribers`() {
+        val server = Server(7000)
+        server.start()
+        val client = Client("localhost", 7000)
+        client.start()
+        client.sync("Test topic").get()
+        client.stop()
+        server.stop()
+    }
+
+    @Test
+    fun `client requests sync for topic with subscriber`() {
+        val topic = "Test topic"
+        val server = Server(7000)
+        server.start()
+        val client1 = Client("localhost", 7000)
+        client1.start()
+        val client2 = Client("localhost", 7000)
+        client2.start()
+        client2.subscribeTopic(topic, {}).get()
+        client1.sync(topic).get()
+        client1.stop()
+        client2.stop()
+        server.stop()
     }
 
     @Test
@@ -31,20 +57,24 @@ class TestClientToServer {
 
         val client1 = Client("localhost", 7000)
         val client2 = Client("localhost", 7000)
+
         client1.start()
         client1.subscribeTopic(topic1) { message ->
+            Thread.sleep(100)  // slow consumption
             q1.add(String(message.getBody()))
-        }
+        }.get()
+
         client2.start()
         client2.subscribeTopic(topic2) { message ->
+            Thread.sleep(100)
             q2.add(String(message.getBody()))
-        }
+        }.get()
 
         // transfer from q1 to q2 via topic
         q1.forEach {
             client1.sendToTopic(topic2, it.toByteArray())
         }
-        Thread.sleep(100)
+        client1.sync(topic2).get()  // wait until all sent messages consumed
         assertEquals(q1, q2)
 
         // transfer back from q2 to q1
@@ -52,7 +82,7 @@ class TestClientToServer {
         q2.forEach {
             client2.sendToTopic(topic1, it.toByteArray())
         }
-        Thread.sleep(100)
+        client2.sync(topic1).get()
         assertEquals(q2, q1)
 
         client1.stop()
